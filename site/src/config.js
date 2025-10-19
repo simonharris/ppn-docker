d441748d2a4a4cce22b569663856c9b59a21e12f
Simon H <github@pointbeing.net>
2025-10-19 21:56:58 +0100

    Various tweaks to get things working

--- site/src/config.js
+++ site/src/config.js
@@ -1,2 +1,3 @@
-export const API_HOST = 'http://192.168.1.73:5000';
-//export const API_HOST = 'https://projects-api.pointbeing.net/';
+//export const API_HOST = 'http://192.168.1.73:5000';
+export const API_HOST = 'https://projects-api.pointbeing.net/';
+//export const API_HOST = 'http://projects-api.localhost/';
